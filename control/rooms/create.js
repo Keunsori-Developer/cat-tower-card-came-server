be31e4f22af66d10295cd3086ab98f066b56ec68
SeungYeon Kim <47077179+seunggom@users.noreply.github.com>
2021-01-30 21:20:30 +0900

    /rooms/create 구현

---
+++ control/rooms/create.js
@@ -0,0 +1,47 @@
+const firebase = require("../../firebaseInitializer.js");
+const randomKey = require('../../utils/randomKey.js');
+const isEmpty = require('../../utils/checkEmpty.js')
+
+module.exports = (req, res) => {
+    let ref = firebase.database.ref("Rooms");
+
+    console.log(req.body);
+    var requestBody = req.body;
+    console.log(requestBody.hostId);
+    console.log(requestBody.name);
+    console.log(requestBody.capacity);
+    console.log(requestBody.mode);
+
+    if (isEmpty(requestBody.hostId)
+        || isEmpty(requestBody.name)
+        || isEmpty(requestBody.capacity)
+        || isEmpty(requestBody.mode)) {
+        res.status(400);
+        res.json(null);
+        console.log("request error");
+
+    }
+    else {
+        var new_roomId = randomKey(5);
+
+        ref.push({
+            roomId: new_roomId,
+            hostId: requestBody.hostId,
+            name: requestBody.name,
+            capacity: requestBody.capacity,
+            mode: requestBody.mode
+        }, function (error) {
+            if (error) {
+                res.status(500);
+                res.json(null);
+            } else {
+                var json = new Object();
+                json.roomId = new_roomId;
+                res.status(200);
+                res.json(JSON.stringify(json));
+                console.log(JSON.stringify(json));
+            }
+        });
+
+    }
+}
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const create = require('../control/rooms/create.js');
+
 const router = express.Router();
 
-router.post('/create', (req, res) => {
+router.post('/create', create);
+
+router.post('/userlist', (req, res) => {
     console.log(req.body);
-    res.status(200);
-    res.json("{\"a\":1}");
+    console.log("아직 미구현");
 });
-router.post('/userlist', (req, res) => {});
-router.post('/active', (req, res) => {});
+router.post('/active', (req, res) => {
+    console.log(req.body);
+    console.log("아직 미구현");
+});
 
 module.exports = router;
