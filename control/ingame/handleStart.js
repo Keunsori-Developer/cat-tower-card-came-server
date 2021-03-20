9db9e1e208117b5ddd6585e35b5743c9743fa408
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2021-03-21 03:19:51 +0900

    create initIngame

--- control/ingame/handleStart.js
+++ control/ingame/handleStart.js
@@ -1,6 +1,8 @@
 const SocketIO = require('socket.io');
 const path = require('path');
 const firebase = require('../../firebaseInitializer.js');
+// const initIngame = require('../../utils/initIngame.js');
+
 
 module.exports = (data, socket) => { 
     console.log(data);
@@ -13,61 +15,14 @@ module.exports = (data, socket) => {
     // let refRooms = firebase.database.ref(`Rooms/${roomId}`);
     socket.join(roomId);
 
+    // initIngame(roomId);
+
     refIngame.once("value",
         (snapshot) => {
             console.log("!!!");
             console.log(snapshot.val());
 
             let {player, suffle, capacity} = snapshot.val();
-            // let lastRound = snapshot.val().round;
-
-            // if(round!==lastRound){
-            //     var newBoard = new Array(57);
-            //     newBoard[0] = 0;
-
-            //     let suffle = [
-            //         "A","A","A","A","A","A","A",
-            //         "B","B","B","B","B","B","B",
-            //         "C","C","C","C","C","C","C",
-            //         "D","D","D","D","D","D","D",
-            //         "E","E","E","E","E","E","E",
-            //         `S${Math.floor(Math.random() * 3)}`];
-
-            //     suffle.sort(()=>{
-            //         return Math.random() - Math.random();
-            //     });
-            //     console.log(suffle);
-
-            //     for(let i in player){
-            //         player[i].giveup == false;
-            //     }
-
-            //     refIngame.update({
-            //         board : Object.assign({}, newBoard),
-            //         order : 0,
-            //         round: round,
-            //         finishCount : 0,
-            //         player,
-            //         suffle
-            //     }, (error) => {
-            //         if (error) {
-            //             console.log("Data could not be saved." + error);
-            //         } else {
-            //             console.log("Data created successfully.");
-            //             try{
-            //                 console.log(player[player.findIndex(findUser)].order);
-            //                 let userOrder = player[player.findIndex(findUser)].order;
-        
-            //                 emitCardgive(capacity, userOrder, suffle);
-            //                 socket.emit("playerorder",JSON.stringify({player}));
-        
-        
-            //             }catch (error) {
-            //                 console.log("error : "+ error);
-            //             }
-            //         }
-            //     });  
-            // } else {
                 try{
                     console.log(player[player.findIndex(findUser)].order);
                     let userOrder = player[player.findIndex(findUser)].order;
@@ -79,7 +34,6 @@ module.exports = (data, socket) => {
                 }catch (error) {
                     console.log("error : "+ error);
                 }
-            // }
         },
         (error) => {
             console.log("The read failed: " + error.code);
--- control/rooms/start.js
+++ control/rooms/start.js
@@ -2,6 +2,7 @@ const firebase = require("../../firebaseInitializer.js");
 const isEmpty = require('../../utils/checkEmpty.js')
 const Enum = require('../../utils/enums.js');
 const jsonConverter = require('../../utils/jsonStringConverter.js');
+const initIngame = require('../../utils/initIngame.js');
 
 module.exports = (req, socket, rooms) => {
     var request = jsonConverter(req);
@@ -35,77 +36,6 @@ module.exports = (req, socket, rooms) => {
                 rooms.to(request.roomId).emit("start", JSON.stringify(response));
 
                 //init ingame db
-                let refIngame = firebase.database.ref(`Ingame/${roomId}`);
-                let refRooms = firebase.database.ref(`Rooms/${roomId}`);
-
-                refRooms.once("value",
-                    (snapshot) => {
-                        console.log("!!!");
-                        console.log(snapshot.val());
-
-                        if(snapshot.val()===null){
-                            console.log("error");
-                            //alert
-                            //return
-                        }
-
-                        let {capacity, joined, mode, name, userList} = snapshot.val();
-
-                        console.log(capacity, joined, mode, name, userList);
-
-                        let suffle = [
-                            "A","A","A","A","A","A","A",
-                            "B","B","B","B","B","B","B",
-                            "C","C","C","C","C","C","C",
-                            "D","D","D","D","D","D","D",
-                            "E","E","E","E","E","E","E",
-                            `S${Math.floor(Math.random() * 3)}`];
-
-                        suffle.sort(()=>{
-                            return Math.random() - Math.random();
-                        });
-                        console.log(suffle);
-
-                        let player = [];
-                        userList.sort(()=>{
-                            return Math.random() - Math.random();
-                        });
-
-                        for (let i in userList) {
-                            player.push(
-                                {
-                                    userInfo : userList[i],
-                                    order : Number(i),
-                                    score : 0,
-                                    giveup : false
-                                }
-                            )
-                        }
-
-                        var newBoard = new Array(57);
-                        newBoard[0] = 0;
-
-                        ref.child(roomId).set({
-                            name : name,
-                            round : 0,
-                            order : 0,
-                            capacity : capacity,
-                            board : Object.assign({}, newBoard),
-                            suffle : suffle,
-                            player : player,
-                            mode : mode,
-                            finishCount : 0
-                        }, (error) => {
-                            if (error) {
-                                console.log("Data could not be saved." + error);
-                            } else {
-                                console.log("Data created successfully.");
-                            }
-                        });
-                    },
-                    (error) => {
-                        console.log("The read failed: " + error.code);
-                    });
-
+                initIngame(request.roomId);
             });
 }
